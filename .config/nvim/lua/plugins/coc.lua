fedc9ee683d0989131c14b6df03f0bb69b4a1434
raph2leretour <tisserandraph@gmail.com>
2025-05-02 15:03:57 +0200

    nvim: coc-nvim conf

--- .config/nvim/lua/plugins/coc.lua
+++ .config/nvim/lua/plugins/coc.lua
@@ -1,7 +1,23 @@
 return {
-  {
-    "neoclide/coc.nvim",
-    branch = "release",
-    build = "npm install",
-  },
+	"neoclide/coc.nvim",
+	branch = "release",
+	build = "npm install",
+	event = { "BufReadPre", "BufNewFile" },
+
+	config = function ()
+		vim.cmd([[
+	  " Utilise <Tab> pour la complétion
+	  inoremap <silent><expr> <TAB> pumvisible() ? "\<C-n>" : "\<TAB>"
+	  inoremap <silent><expr> <S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+
+	  " Valide la sélection avec Enter
+	  inoremap <silent><expr> <CR> pumvisible() ? coc#_select_confirm() : "\<CR>"
+
+	  " Jump vers les définitions
+	  nmap <silent> gd <Plug>(coc-definition)
+	  nmap <silent> gy <Plug>(coc-type-definition)
+	  nmap <silent> gi <Plug>(coc-implementation)
+	  nmap <silent> gr <Plug>(coc-references)
+	  ]])
+	end
 }
