4f7ba92228e047a7a43727a8da24796690377bb9
raph2leretour <tisserandraph@gmail.com>
2025-10-28 15:24:59 +0100

    nvim: fixing <C-k> to switch windows

--- .config/nvim/lua/plugins/lsp/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp/lsp-config.lua
@@ -25,7 +25,7 @@ return {
 				buf_set_keymap("n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
 				buf_set_keymap("n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 				buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-				buf_set_keymap("n", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+				-- buf_set_keymap("n", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 				buf_set_keymap("n", "<space>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
 				buf_set_keymap("n", "<space>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 			end,
