4e0bd821b184a389a44f72f9b05cddd191f88b8e
raph2leretour <tisserandraph@gmail.com>
2025-11-25 18:40:53 +0100

    nvim: coc-nvim: changed autocompletion keys

--- .config/nvim/lua/plugins/coc.lua
+++ .config/nvim/lua/plugins/coc.lua
@@ -4,20 +4,20 @@ return {
 	build = "npm install",
 	event = { "BufReadPre", "BufNewFile" },
 
-	config = function ()
+	config = function()
 		vim.cmd([[
-	  " Utilise <Tab> pour la complétion
-	  inoremap <silent><expr> <TAB> pumvisible() ? "\<C-n>" : "\<TAB>"
-	  inoremap <silent><expr> <S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+			" Utilise <Tab> pour la complétion
+			inoremap <silent><expr> <C-j> pumvisible() ? "\<C-n>" : "\<TAB>"
+			inoremap <silent><expr> <C-k> pumvisible() ? "\<C-p>" : "\<C-h>"
 
-	  " Valide la sélection avec Enter
-	  inoremap <silent><expr> <CR> pumvisible() ? coc#_select_confirm() : "\<CR>"
+			" Valide la sélection avec Enter
+			inoremap <silent><expr> <TAB> pumvisible() ? coc#_select_confirm() : "\<CR>"
 
-	  " Jump vers les définitions
-	  nmap <silent> gd <Plug>(coc-definition)
-	  nmap <silent> gy <Plug>(coc-type-definition)
-	  nmap <silent> gi <Plug>(coc-implementation)
-	  nmap <silent> gr <Plug>(coc-references)
-	  ]])
+			" Jump vers les définitions
+			nmap <silent> gd <Plug>(coc-definition)
+			nmap <silent> gy <Plug>(coc-type-definition)
+			nmap <silent> gi <Plug>(coc-implementation)
+			nmap <silent> gr <Plug>(coc-references)
+		]])
 	end
 }
