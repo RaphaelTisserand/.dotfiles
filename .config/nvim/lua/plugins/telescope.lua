487046b5170677ea7f2a8c85e656703c2a2cdee9
Raphael Tisserand Pro <raphaelPro@mac-7.home>
2026-02-03 11:26:46 +0100

    nvim: plugin: telescope: removed the «branch»

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
+	-- branch = "0.2.1",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
